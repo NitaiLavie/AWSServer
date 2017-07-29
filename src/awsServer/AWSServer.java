7c99a43c53f3e04bc58c8e5214f2b79d26b28f4a
NitaiLavie <NitaiLavie@users.noreply.github.com>
2017-07-29 13:24:18 +0300

    - added running loop to server - creating new ClientTask with each connection

--- src/awsServer/AWSServer.java
+++ src/awsServer/AWSServer.java
@@ -2,19 +2,39 @@ package awsServer;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class AWSServer {
 	
+	private static final int mServerIP = 7777;
 	
 	public static void main(String [] args) {
-		try {
-			ServerSocket serverSocket = new ServerSocket(8060);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		boolean run = true;
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+			try {
+				serverSocket = new ServerSocket(mServerIP);
+				while(run) {
+					clientSocket = serverSocket.accept();
+					new ClientTask(clientSocket).run();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if(serverSocket!=null) {
+						serverSocket.close();
+					}
+					if(clientSocket!=null) {
+						clientSocket.close();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 	}
 	
 }
---
+++ src/awsServer/ClientTask.java
@@ -0,0 +1,16 @@
+package awsServer;
+
+import java.net.Socket;
+
+public class ClientTask {
+
+	public ClientTask(Socket clientSocket) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
